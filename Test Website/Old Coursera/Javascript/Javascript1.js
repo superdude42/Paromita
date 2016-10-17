d2a2c502a9bc2c636c58504b14f57f8ff0003049
superdude42 <derek.c.johnson42@gmail.com>
2016-10-17 16:05:34 +0530

    add all

---
+++ Test Website/Old Coursera/Javascript/Javascript1.js
@@ -0,0 +1,13 @@
+
+function displayDate(){
+	document.getElementById("cal").innerHTML=Date();
+}
+function message(){
+	document.getElementById("cal").innerHTML=Date();
+}
+function displayId(element){
+	alert(element.alt);
+}
+function showProperties(element){
+	document.getElementById('a2').innerHTML= element.id;
+}
